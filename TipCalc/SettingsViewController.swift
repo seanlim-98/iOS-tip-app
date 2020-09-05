85a422efdf18c5c64b8d43f9fef9b8bf03d4adec
Sean Lim <seanlim@resnet157-99.medford.tufts.edu>
2020-09-05 19:31:32 -0400

    Added comments, fixed dark mode persistence bug
    Signed-off-by: Sean Lim <seanlim@resnet157-99.medford.tufts.edu>

--- TipCalc/SettingsViewController.swift
+++ TipCalc/SettingsViewController.swift
@@ -17,7 +17,6 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet var currencyOptions: [UIButton]!
     
-    // Deselects settings once others are chosen
     func resetButtonStates(tagNum: intmax_t, btnList: [UIButton]) {
         for button in btnList {
             if(button.tag != tagNum) {
@@ -26,23 +25,21 @@ class SettingsViewController: UIViewController {
         }
     }
     
-    // Select tip percentage
     @IBAction func btnTapped(_ sender: UIButton) {
-        let selectedPercTagNum = sender.tag
-        resetButtonStates(tagNum: selectedPercTagNum, btnList: self.buttons) // update button state
+        let selectedTagNum = sender.tag
+        resetButtonStates(tagNum: selectedTagNum, btnList: self.buttons)
         if let button = sender as? UIButton {
             if !button.isSelected {
                 button.isSelected = true
             }
         }
-        defaults.set(selectedPercTagNum, forKey: "selected")
+        defaults.set(selectedTagNum, forKey: "selected")
         defaults.synchronize()
     }
     
-    // Select currency
     @IBAction func selectCurrency(_ sender: UIButton) {
         let selectedCurrTag = sender.tag
-        resetButtonStates(tagNum: selectedCurrTag, btnList: self.currencyOptions) // update button state
+        resetButtonStates(tagNum: selectedCurrTag, btnList: self.currencyOptions)
         if let button = sender as? UIButton {
             if !button.isSelected {
                 button.isSelected = true
@@ -52,18 +49,14 @@ class SettingsViewController: UIViewController {
         defaults.synchronize()
     }
     
-    // data persistence
     override func viewWillAppear(_ animated: Bool) {
         self.buttons[defaults.integer(forKey: "selected")].isSelected = true;
         self.darkModeSwitch.isOn = defaults.bool(forKey:"DarkModeOn")
-        overrideUserInterfaceStyle = defaults.bool(forKey: "DarkModeOn") == true ?  .dark : .light
         self.currencyOptions[defaults.integer(forKey: "currency")].isSelected = true;
     }
     
-    // Toggle dark mode switch
     @IBAction func darkModeToggle(_ sender: UISwitch) {
         sender.isOn == true ? defaults.set(true, forKey: "DarkModeOn") : defaults.set(false, forKey: "DarkModeOn")
-        overrideUserInterfaceStyle = defaults.bool(forKey: "DarkModeOn") == true ?  .dark : .light
         defaults.synchronize()
     }
     
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -13,28 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
+    
     var old_curr = 0
     let defaults = UserDefaults.standard;
     let curr_weight = [1, 0.75, 0.84]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Tip Calculator"
     }
     
+    // Currency conversion for existing text-field data
     func currency_conversion() {
         let bill_temp = Double(billAmountTextField.text!) ?? 0
         let converted_value = bill_temp * Double(curr_weight[defaults.integer(forKey:"currency")]/curr_weight[old_curr])
         billAmountTextField.text = String(format: "%.2f", converted_value)
-        calculateTip(self)
+        calculateTip(self) // call calculation function after initial conversion
     }
     
-    
+    // Render changes after settings are altered
     override func viewWillAppear(_ animated: Bool) {
         currency_conversion()
         overrideUserInterfaceStyle = defaults.bool(forKey: "DarkModeOn") == true ?  .dark : .light
-        
     }
     
+    // Tip calculation function, append currency symbol
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15,0.18,0.20]
